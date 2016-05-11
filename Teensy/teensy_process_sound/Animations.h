b3b3a2bc87d78f3c82985c878f2e72e1d8e89ac4
davidbrai <davidbrai@gmail.com>
2016-05-11 14:00:25 +0300

    small changes

--- Teensy/teensy_process_sound/Animations.h
+++ Teensy/teensy_process_sound/Animations.h
@@ -4,6 +4,10 @@
 uint8_t hueShift = 0;
 
 uint8_t getHue(uint8_t hue) {
+//  EVERY_N_MILLIS(150) {
+//    hueShift++;
+//  }
+  
   return hue + hueShift;
 }
 
@@ -14,7 +18,7 @@ void shiftArrayRight(CRGB leds[], int moveBy, int startIndex) {
 }
 
 void moveStrips(CRGB leds[], int startIndex) {
-  EVERY_N_MILLIS(5) {
+  EVERY_N_MILLIS(5*6) {
     for (int i = 0; i < NUM_STRIPS; i++) {
       shiftArrayRight(&leds[i*NUM_LEDS_PER_STRIP], 1, startIndex);
     }
@@ -34,9 +38,9 @@ uint8_t getValue(int value) {
 
 void bandsAnimation(float bands[], CRGB leds[]) {
   leds[0] = CHSV(getHue(0), 150, getValue(255*bands[0]*2));
-  leds[NUM_LEDS_PER_STRIP] = CHSV(getHue(43), 150, getValue(255*bands[1]));
-  leds[NUM_LEDS_PER_STRIP*2] = CHSV(getHue(170), 150, getValue(255*(bands[3]+bands[2])));
-  leds[NUM_LEDS_PER_STRIP*3] = CHSV(getHue(213), 150, getValue(255*(bands[4]+bands[5])));
+  leds[NUM_LEDS_PER_STRIP] = CHSV(getHue(43), 200, getValue(255*bands[1]));
+  leds[NUM_LEDS_PER_STRIP*2] = CHSV(getHue(170), 200, getValue(255*(bands[3]+bands[2])));
+  leds[NUM_LEDS_PER_STRIP*3] = CHSV(getHue(213), 200, getValue(255*(bands[4]+bands[5])));
 
   mirrorStrips(leds);
   moveStrips(leds, 0);
